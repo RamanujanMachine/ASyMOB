188bb70d665beb6b971440c73b9883d5b8c1203b
Rotem Elimelech <rotelimelech@gmail.com>
2025-04-15 13:06:13 +0300

    using light llm instead of implementing things alone

--- llm_interface.py
+++ llm_interface.py
@@ -1,38 +1,25 @@
-from abc import abstractmethod
+from litellm import completion
 import json
 
 
-class LLMInterface:
-    def __init__(self, api_key: str = None):
-        pass
-
-    @abstractmethod
-    def write(self):
-        """
-        Write a message to the language model.
-        """
-        pass
+def _get_api_key(provider):
+    with open('api_keys.json', 'r') as f:
+        keys = json.load(f)
     
-    @abstractmethod
-    def read(self):
-        """
-        Read a message from the language model.
-        """
-        pass
+    if provider not in keys:
+        raise ValueError("API key not found in api_key.txt")
+    
+    return keys[provider]
 
-    @abstractmethod
-    def chat(self):
-        """
-        Write a single message and read the response from the language model.
-        """
-        pass
 
-    def _load_api_key(self, api_key: str = None, provider: str = None) -> str:
-        if api_key is None:
-            with open('api_keys.json', 'r') as f:
-                keys = json.load(f)
-            if provider not in keys:
-                raise ValueError("API key not found in api_key.txt and "
-                                 "was not provided.")
-            api_key = keys['gemini']
-        self.api_key = api_key
+def send_and_receive_message(model, message):
+    provider = model.split('/')[0]
+    api_key = _get_api_key(provider)
+    messages = [
+        {"role": "user", "content": message}
+    ]
+    response = completion(
+        model=model, 
+        api_key=api_key, 
+        messages=messages)
+    return response.choices[0].message.content
