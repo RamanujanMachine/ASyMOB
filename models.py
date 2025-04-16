6793e336eb0086f36525b9d7b109e399f93fc1e8
Rotem Elimelech <rotelimelech@gmail.com>
2025-04-16 12:08:55 +0300

    Placing o3 as the first model

---
+++ models.py
@@ -0,0 +1,9 @@
+MODELS = [
+    "openai/o3-mini",
+    "openai/gpt-4o",
+    "openai/gpt-4o-mini",
+
+    "gemini/gemini-2.0-flash",
+    "gemini/gemini-2.0-flash-thinking-exp",
+    "gemini/gemini-2.5-pro-exp-03-25"
+]
