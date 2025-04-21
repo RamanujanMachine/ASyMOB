9c65ac029d4f96ed1e0eae3155e36a7bbf4fb7f5
Rotem Elimelech <rotelimelech@gmail.com>
2025-04-21 18:52:13 +0300

    using newer models

--- models.py
+++ models.py
@@ -3,11 +3,16 @@ from gemini_interface import GeminiInterface
 from openai_interface import OpenAIInterface
 
 MODELS = {
-    "openai/o3-mini": OpenAIInterface("o3-mini"),
-    "openai/gpt-4o": OpenAIInterface("gpt-4o"),
-    "openai/gpt-4o-mini": OpenAIInterface("gpt-4o-mini"),
-    
+    # "openai/o3-mini": OpenAIInterface("o3-mini"),
+    # "openai/gpt-4o": OpenAIInterface("gpt-4o"),
+    # "openai/gpt-4o-mini": OpenAIInterface("gpt-4o-mini"),
+
+
     "gemini/gemini-2.0-flash": GeminiInterface("gemini-2.0-flash"),
-    "gemini/gemini-2.0-flash-thinking-exp": GeminiInterface("gemini-2.0-flash-thinking-exp"),
-    "gemini/gemini-2.5-pro-exp-03-25": GeminiInterface("gemini-2.5-pro-exp-03-25"),
+    "gemini/gemini-2.0-flash-lite": GeminiInterface("gemini-2.0-flash-lite"),
+
+    "gemini/gemini-2.5-flash-preview-04-17": GeminiInterface("gemini-2.5-flash-preview-04-17"),
+    "gemini/gemini-2.5-pro-preview-03-25": GeminiInterface("gemini-2.5-pro-preview-03-25"),
+
+    "gemini/gemma-3-27b-it": GeminiInterface("gemma-3-27b-it"),
 }
