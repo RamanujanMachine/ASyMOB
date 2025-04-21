5415f59479e7ad97f4ef95df924aa6371fd97781
Rotem Elimelech <rotelimelech@gmail.com>
2025-04-21 18:51:44 +0300

    Cleaningup, not running llm latex2sympy anymore

--- survey_llm_answers.py
+++ survey_llm_answers.py
@@ -9,10 +9,11 @@ import pandas as pd
 from sp_vars import *
 import re
 
+
 QUESTIONS_PATH = 'questions.json'
 # SYMPY_CONVERTER_MODEL = 'openai/gpt-4o'# 'gemini/gemini-2.5-pro-exp-03-25'
 # SYMPY_CONVERTER = GeminiInterface("gemini-2.5-pro-exp-03-25") # ge(SYMPY_CONVERTER_MODEL)
-SYMPY_CONVERTER = OpenAIInterface("o3-mini")
+SYMPY_CONVERTER = OpenAIInterface("gpt-4o")
 MATH_INSTRUCTIONS = (
     'Finish your answer by writing "The final answer is:" and then the '
     'answer in latex in a new line. use $$ to wrap over the latex text. '
@@ -47,6 +48,9 @@ def extract_latex_answer(textual_answer):
         re.DOTALL)
     if match:
         latex_answer = match.group(1).strip()
+        # clean up the latex answer
+        latex_answer = latex_answer.replace(r'\displaystyle', '')
+        latex_answer = latex_answer.replace(r'\dots', '')
         return latex_answer
     else:
         raise ValueError("No latex answer found in the textual answer.")
@@ -59,31 +63,35 @@ def ask_model(model, question_text, code_execution):
     Returns a sympy object extracted from the textual answer, the sympy 
     expression as a string and the textual answer itself.
     """
-    textual_answer = model.send_message(
-        message=MATH_INSTRUCTIONS + question_text,
-        code_execution=code_execution,
-    )
+    try:
+        textual_answer = model.send_message(
+            message=MATH_INSTRUCTIONS + question_text,
+            code_execution=code_execution,
+        )
+    except Exception as e:
+        print(f"Error sending message to model: {e}")
+        print('message:', MATH_INSTRUCTIONS + question_text)
+        return {}
+    
+    result = {
+        'full_answer': textual_answer,
+    }
     try:
         # extract the final answer from the textual answer
         latex_answer = extract_latex_answer(textual_answer)
+        result['final_answer_latex'] = latex_answer
         
         # convert the latex answer to a sympy expression
-        sp_deter = math_parsers.latex_to_sympy_deter(latex_answer)
-        sp_llm = math_parsers.latex_to_sympy_llm(latex_answer)
+        result['sp_deter'] = math_parsers.latex_to_sympy_deter(latex_answer)
+        # result['sp_llm'] = math_parsers.latex_to_sympy_llm(latex_answer)
+        result['sp_llm'] = None
     except Exception as e:
         print(f"Error processing the answer: {e}")
         print(f"Textual answer: {textual_answer}")
-        sp_deter = None
-        sp_llm = None
-        latex_answer = None
+        result['sp_deter'] = None if 'sp_deter' not in result else result['sp_deter']
+        result['sp_llm'] = None if 'sp_llm' not in result else result['sp_llm']
     
-    return {
-        'full_answer': textual_answer,
-        'final_answer_latex': latex_answer,
-        'sp_deter': sp_deter,
-        'sp_llm': sp_llm,
-    }
-
+    return result
 
 def symbolic_comparison(A, B):
     """
@@ -121,12 +129,12 @@ def main():
                             code_execution=code_execution)
                     })
 
-    df = pd.DataFrame.from_records(results)
-    df.to_excel(
-        'results.xlsx', 
-        index=False, 
-        sheet_name='results'
-    )
+        df = pd.DataFrame.from_records(results)
+        df.to_excel(
+            'results.xlsx', 
+            index=False, 
+            sheet_name='results'
+        )
 
 
 if __name__ == "__main__":
