26db1f36f5dce6d0e7c33a6558d66036a79f3b42
Rotem Elimelech <rotelimelech@gmail.com>
2025-04-22 13:22:59 +0300

    adding groq hosted models

---
+++ groq_interface.py
@@ -0,0 +1,49 @@
+from groq import Groq
+from llm_interface import GenericLLMInterface, USE_CODE_PREFIX, NO_CODE_PREFIX
+
+class GroqInterface(GenericLLMInterface):
+    """
+    A class to interact specifically with models over Groq.
+
+    Inherits from GenericLLMInterface but overrides send_message to use
+    the groq API.
+    """
+    def __init__(self, model):
+        self.model = model
+        self._load_api_key(provider='groq')
+        self.client = Groq(api_key=self.api_key)
+
+    def send_message(self, message, code_execution=False, 
+                     max_completion_tokens=50_000):
+        message = self._incentivize_code_execution(
+            message, use_code=code_execution)
+        print(f"Sending message to Groq model: {self.model}")
+        print(f"Message: {message}")
+
+        # Some models have a max token limit.
+        if self.model == 'meta-llama/llama-4-scout-17b-16e-instruct':
+            if max_completion_tokens > 8192:
+                max_completion_tokens = 8192
+        elif self.model == 'meta-llama/llama-4-maverick-17b-128e-instruct': 
+            if max_completion_tokens > 8192:
+                max_completion_tokens = 8192
+
+        completion = self.client.chat.completions.create(
+            model=self.model,
+            messages=[{
+                    "role": "user",
+                    "content": message}],
+            temperature=0.6,
+            max_completion_tokens=max_completion_tokens,
+            top_p=0.95,
+            stream=True,
+            stop=None,
+        )
+
+        response = ''
+        for chunk in completion:
+            if chunk.choices[0].delta.content is None:
+                continue
+            response += chunk.choices[0].delta.content
+
+        return response
--- models.py
+++ models.py
@@ -1,18 +1,33 @@
 from llm_interface import GenericLLMInterface
 from gemini_interface import GeminiInterface
 from openai_interface import OpenAIInterface
+from groq_interface import GroqInterface
+
 
 MODELS = {
-    # "openai/o3-mini": OpenAIInterface("o3-mini"),
-    # "openai/gpt-4o": OpenAIInterface("gpt-4o"),
-    # "openai/gpt-4o-mini": OpenAIInterface("gpt-4o-mini"),
+    # OpenAI models
+    "openai/o3-mini": OpenAIInterface("o3-mini"),
+    "openai/gpt-4o": OpenAIInterface("gpt-4o"),
+    "openai/gpt-4o-mini": OpenAIInterface("gpt-4o-mini"),
 
+    # Google Gemini models
+    "gemini/gemini-2.0-flash": \
+        GeminiInterface("gemini-2.0-flash"),
+    "gemini/gemini-2.0-flash-lite": \
+        GeminiInterface("gemini-2.0-flash-lite"),
+    "gemini/gemini-2.5-flash-preview-04-17": \
+        GeminiInterface("gemini-2.5-flash-preview-04-17"),
+    "gemini/gemini-2.5-pro-preview-03-25": \
+        GeminiInterface("gemini-2.5-pro-preview-03-25"),
+    "gemini/gemma-3-27b-it": \
+        GeminiInterface("gemma-3-27b-it"),
 
-    "gemini/gemini-2.0-flash": GeminiInterface("gemini-2.0-flash"),
-    "gemini/gemini-2.0-flash-lite": GeminiInterface("gemini-2.0-flash-lite"),
-
-    "gemini/gemini-2.5-flash-preview-04-17": GeminiInterface("gemini-2.5-flash-preview-04-17"),
-    "gemini/gemini-2.5-pro-preview-03-25": GeminiInterface("gemini-2.5-pro-preview-03-25"),
-
-    "gemini/gemma-3-27b-it": GeminiInterface("gemma-3-27b-it"),
+    # Groq models
+    'qwen-qwq-32b' : GroqInterface("qwen-qwq-32b"),
+    'meta-llama/llama-4-maverick-17b-128e-instruct': \
+        GroqInterface("meta-llama/llama-4-maverick-17b-128e-instruct"),
+    'meta-llama/llama-4-scout-17b-16e-instruct': \
+        GroqInterface("meta-llama/llama-4-scout-17b-16e-instruct"),
+    'deepseek-r1-distill-llama-70b': \
+        GroqInterface("deepseek-r1-distill-llama-70b"),
 }
