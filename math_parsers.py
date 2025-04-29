b49a5e88266b30a276bdcf23bc61b4644bc9e134
Rotem Elimelech <rotelimelech@gmail.com>
2025-04-29 14:37:48 +0300

    perfomring latex to sympy using deterministic method, and rolling back to LLM upon failure

--- math_parsers.py
+++ math_parsers.py
@@ -1,4 +1,5 @@
 import re
+import inspect
 from sp_vars import *
 import sympy as sp
 import pandas as pd
@@ -8,7 +9,15 @@ from openai_interface import OpenAIInterface
 from gemini_interface import GeminiInterface
 
 SYMPY_CONVERTER = GeminiInterface("gemini-2.0-flash")
-
+KNOWN_FUNCTIONS = {
+    'atan': sp.atan,
+    'atan2': sp.atan2,
+    'arctan': sp.atan,
+    'O': sp.O,
+    'gamma': sp.gamma,
+    'Gamma': sp.gamma,
+    'exp': sp.exp,
+}
 
 def remove_equality(sp_expr):
     try:
@@ -30,7 +39,10 @@ def parse_sympy_str(expr_str):
             transformations=(
                 standard_transformations + (implicit_multiplication_application,)
             )
-        )))
+        ))).subs({
+            sp.var('e'): sp.exp(1),
+            sp.var('pi'): sp.pi
+        })
     except Exception as e:
         print(expr_str)
         raise e
@@ -43,6 +55,7 @@ def latex_to_sympy_deter(latex_str):
     if pd.isna(latex_str):
         return pd.NA
     try:
+        # Latex cleanup
         latex_str = re.sub(
             r'\\(?:left|big|Big|Biggl|Bigl)' # Non-consuming left indicator
             r'[\[\(\|\{]', 
@@ -56,18 +69,22 @@ def latex_to_sympy_deter(latex_str):
         
         # Fix the latex string to be compatible with sympy
         latex_str = re.sub(
-            r'(?<!\\)atan',
-            '\\atan',
-            latex_str)
-        
-        # Fix the latex string to be compatible with sympy
-        latex_str = re.sub(
             r'(\\operatorname{.*?})',
             lambda match: '\\' + match.group(0)[:-1].split('{')[1],
             latex_str)
         
-
-        return remove_equality(fix_expr(parse_latex(latex_str)))
+        # Fix the latex string to be compatible with sympy
+        latex_str = re.sub(
+            r'(?<!\\)atan',
+            r'\\atan',
+            latex_str)
+        
+        # use constants for e and pi
+        sp_expr = remove_equality(fix_expr(parse_latex(latex_str)))
+        return sp_expr.subs({
+            sp.var('e'): sp.exp(1),
+            sp.var('pi'): sp.pi
+        })
     except Exception as e:
         print('Error parsing latex string:')
         print(latex_str)
@@ -80,19 +97,22 @@ def latex_to_sympy_llm(latex_str):
     Convert a LaTeX string to a sympy expression, using the LLM parser.
     """
     function_def = SYMPY_CONVERTER.send_message(
-        "Following is a mathematical expression written in latex. Please " 
-        "create a python function that recreates this expression using " 
-        "sympy, exactly as it is written in the expression given. The " 
-        "function's signature will be `solution(x, n, k, A, B, C, D, E, F)` "
-        "and it will return the expression as a sympy object. The " 
-        "implementation will be identical to the expression given. You will " 
-        "not use any form of simplification or modification for the " 
-        "mathematical expression. If the answer is not dependent on one "
-        "or more of the parameters accepted it may not use them. Only print "
-        "the function, without additional text. Assume all necessary "
-        "parameters  and symbols already exists. Access sympy functions "
-        "through the sp module (e.g. sp.hyper). Before returning the function"
-        "run your code to make sure that your function is valid\n\n" + latex_str
+        message="Following is a mathematical expression written in latex. "
+                "Please create a python function that recreates this "
+                "expression using sympy, exactly as it is written in the "
+                "expression given. The function's signature will be "
+                "solution(...) and it will accept as parameters all of the "
+                "variables and constant in the expression you are given. It "
+                "will return the expression as a sympy object. The "
+                "implementation will be identical to the expression given. "
+                "You will not use any form of simplification or modification "
+                "for the mathematical expression. If the answer is not "
+                "dependent on one or more of the parameters accepted it may "
+                "not use them. Only print the function, without additional "
+                "text. Assume all necessary parameters and symbols already "
+                "exists. Access sympy functions through the sp module "
+                "(e.g. sp.hyper). Do not use sp.Rational, use the division "
+                "operator (e.g. /). \n\n" + latex_str
         )
     
     print(function_def)
@@ -102,10 +122,34 @@ def latex_to_sympy_llm(latex_str):
 
     exec(function_def, globals())
     
+    # Dynamically extract the function's arguments and pass what is needed
+    sig = inspect.signature(solution)
+    accepted_params = sig.parameters
+    filtered_args = {k: v for k, v in var_mapping.items() if k in accepted_params}
+
     # Function solution defined dynamically
     return solution(
-        x, n, k, A, B, C, D, E, F
+        **filtered_args
     )
+
+def latex_to_sympy(latex_str, return_type=True):
+    """
+    Convert a LaTeX string to a sympy expression, using the LLM parser.
+    """
+    if pd.isna(latex_str):
+        return pd.NA, 'na'
+    try:
+        sp_expr = latex_to_sympy_deter(latex_str)
+        if (not pd.isna(sp_expr) and 
+            sp_expr.free_symbols.issubset(set(var_mapping.values()))): 
+            return sp_expr, 'deterministic'
+        else:
+            return latex_to_sympy_llm(latex_str), 'llm'
+    except Exception as e:
+        print('Error parsing latex string:')
+        print(latex_str)
+        print(e)
+        return pd.NA, 'error'
 
 
 def format_final_answer_to_sympy(textual_answer):
@@ -133,6 +177,8 @@ def fix_expr(expr,):
         if str(expr) == 'E':
             # If not, it will often return the constant e
             return E
+        if expr == sp.E:
+            return e
         return expr
     
     # False identification of var as string
@@ -144,8 +190,8 @@ def fix_expr(expr,):
             fix_expr(expr.args[0])
             )
     
-    if str(expr.func) == 'O':
-        return sp.O(
+    if str(expr.func) in KNOWN_FUNCTIONS:
+        return KNOWN_FUNCTIONS[str(expr.func)](
             *[fix_expr(arg) for arg in expr.args]
         )
 
