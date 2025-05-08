814e3c7b2ade919d4bab83ad82c858e66c9f8b79
Rotem Elimelech <rotelimelech@gmail.com>
2025-05-08 19:54:08 +0300

    unused files

--- test_models.py
+++
@@ -1,7 +0,0 @@
-from llm_interface import send_and_receive_message
-from models import MODELS
-
-
-for model in MODELS:
-    response = send_and_receive_message(model, "Hello, how are you?")
-    print(model, response)
