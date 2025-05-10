f829da3360dcf91326c85f82b7f150cb9459e0ae
Rotem Elimelech <rotelimelech@gmail.com>
2025-05-10 23:24:54 +0300

    other filters

--- create_subs.py
+++ create_subs.py
@@ -10,20 +10,19 @@ sys.set_int_max_str_digits(10_000_000)
 
 N_SUBS = 5
 N_TRIES = 200
-QUESTIONS_FILE = '11_5K_questions.json'
-OUTPUT_FILE = '11_5K_questions_subs.json'
+QUESTIONS_FILE = 'questions.json'
+OUTPUT_FILE = 'SymQLst-Eq-Num-Sym-07_05_subs.json'
 VAR_SUBSTITUTIONS = {
     A: lambda: np.random.randint(1, 10),
     B: lambda: np.random.randint(1, 10),
     D: lambda: np.random.randint(1, 10),
-    E: lambda: np.random.randint(1, 10),
     F: lambda: np.random.randint(1, 10),
     G: lambda: np.random.randint(1, 10),
     H: lambda: np.random.randint(1, 10),
     J: lambda: np.random.randint(1, 10),
     K: lambda: np.random.randint(1, 10),
     n: lambda: np.random.randint(1, 10),
-    x: lambda: np.random.randn()*10,
+    x: lambda: np.abs(np.random.randn()*10),
     sp.var('e'): lambda: sp.exp(1), # e is not a variable, but a constant
     # sp.var('pi'): lambda: sp.pi, # pi is not a variable, but a constant
 
@@ -49,10 +48,20 @@ def _filter_large_powers(question):
         5387, 5388, 5389, 5390, 5436, 5437, 5438, 5439, 5440, 6528, 6529, 6530,
         6531, 6532, 7104, 7105, 7106, 7107, 7108, 10427, 10428, 10429, 10430,
         10431, 10432, 10422, 9121, 9122, 9123, 9124, 9125, 9126, 9151, 9152, 
-        9153, 9154, 9155, 9156]: 
+        9153, 9154, 9155, 9156, 7935, 7936, 7937, 7938, 7984, 7985, 7986,
+        7987, 7988, 9076, 9077, 9078, 9079, 9080, 9652, 9653, 9654, 9655, 9656,
+        12970, 12977, 12978, 12979, 12980, 11670, 11671, 11672, 11673, 11674,
+        11700]: 
         return False
     if 'Numeric' not in question['Variation']:
         return True
+    numeric_level = question['Variation'].split('-')[-1]
+    if numeric_level == 'S':
+        # S means symbolic, so we can skip it
+        return True
+    numeric_level = int(numeric_level)
+    if numeric_level >= 7:
+        return False
     unevaluated = sp.parse_expr(question['Answer in Sympy'], evaluate=False)
     try:
         if unevaluated.has(sp.Pow):
