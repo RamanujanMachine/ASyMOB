0b01a179e1fc16c9a035d84ebf9437fab49f3ba9
Rotem Elimelech <rotelimelech@gmail.com>
2025-09-24 12:36:05 +0300

    added feature that reuses containers

--- openai_interface.py
+++ openai_interface.py
@@ -1,6 +1,10 @@
 from llm_interface import GenericLLMInterface
 import openai
 from openai import OpenAI
+import uuid
+
+TIMEOUT = 60
+REUSE_CONTAINER = True
 
 class OpenAIInterface(GenericLLMInterface):
     """
@@ -11,6 +15,7 @@ class OpenAIInterface(GenericLLMInterface):
         self.model = model
         self._load_api_key(provider='openai')
         self.client = OpenAI(api_key=self.api_key)
+        self.container = None
 
     def send_message(self, message, code_execution=False, return_extra=False):
         """
@@ -31,13 +36,52 @@ class OpenAIInterface(GenericLLMInterface):
                 input=message
             )
         else:
-            response = response = self.client.responses.create(
-                model=self.model,
-                input=message,
-                tools=[{
-                    'type': 'code_interpreter', 
-                    'container': {'type': 'auto'}}]
-            )
+            if REUSE_CONTAINER:
+                if self.container is None:
+                    self.container = self.client.containers.create(
+                        name=f"code-interpreter-{str(uuid.uuid4())}",
+                    )
+                    print(f"Created container {self.container.id}")
+                else:
+                    print(f"Reusing container {self.container.id}")
+                container = self.container
+            else:
+                container = self.client.containers.create(
+                    name=f"code-interpreter-{str(uuid.uuid4())}",
+                )
+
+            try:
+                response = self.client.responses.create(
+                    model=self.model,
+                    input=message,
+                    tools=[{
+                        'type': 'code_interpreter', 
+                        # 'container': {'type': 'auto'}}]
+                        'container': container.id}],
+                    timeout=TIMEOUT
+                )
+            except openai.OpenAIError as e:
+                # for every error, delete the container and raise
+                print(f'error during code execution: {e}')
+                print(f'cleaning up container {container.id}')
+                if REUSE_CONTAINER:
+                    self.container = None
+                raise e
+
+        code_interpreter_ids = set()
+        for p in response.output:
+            if p.type == 'code_interpreter_call':
+                code_interpreter_ids.add(p.container_id)
+        if len(code_interpreter_ids) > 1:
+            import json
+            error_data = {
+                'message': message,
+                'model': self.model,
+                'code_execution': code_execution,
+                'response': response.to_dict(),
+            }
+            with open('openai_multiple_code_interpreter.json', 'w') as f:
+                json.dump(error_data, f, indent=2)
 
         if return_extra:
             code_used = any([
