f4382ef4b06fa5fa065355a475d414dad1894eb2
Rotem Elimelech <rotelimelech@gmail.com>
2025-09-24 12:35:40 +0300

    gemini-2.5 is no longer in preview

--- gemini_interface.py
+++ gemini_interface.py
@@ -5,7 +5,8 @@ from llm_interface import GenericLLMInterface
 
 CODE_RUNNING_MODELS = [
     'gemini-2.0-flash',
-    'gemini-2.5-flash-preview-04-17',
+    # 'gemini-2.5-flash-preview-04-17',
+    'gemini-2.5-flash',
     'gemini-2.5-pro-preview-03-25',
     # gemma-3-27b-it does not support code execution
 ]
